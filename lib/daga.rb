977da3fa812e5d21aec823f5012acbafcd87e0b5
Tommaso Patrizi <tommasop@gmail.com>
2015-10-16 12:18:55 +0200

    in json

--- lib/daga.rb
+++ lib/daga.rb
@@ -46,8 +46,8 @@ module Daga
       user.auth_user_id = SecureRandom.uuid
       user.save
       token = AuthToken.encode({ auth: user.auth_user_id, scopes: user.scopes }, @secret)
-      payload = Oj.dump(token)
-      Rack::Response.new([id_token: payload], 201).finish
+      payload = Oj.dump(id_token: token)
+      Rack::Response.new([payload], 201).finish
     end
 
     def login(username, password)
