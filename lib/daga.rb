e77bd18b79adaf9c7d5f1162312e8f3b3de20638
Tommaso Patrizi <tpatrizi@makeplan.it>
2016-07-27 17:57:48 +0200

    debugging

--- lib/daga.rb
+++ lib/daga.rb
@@ -31,15 +31,14 @@ module Daga
 
     def call(env)
       req = Rack::Request.new(env)
-      puts req
+      puts env
+      puts req.body
 
       if req.post? && req.path_info == @url
         login_data = req.body.read ? Oj.load( req.body.read ) : nil
-        puts login_data
         if login_data
           login(login_data[:email], login_data[:password])
         else
-          puts req.params
           login(req.params["username"], req.params["password"])
         end
       else
